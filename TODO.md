36c703956490948de3f7033588ec99d39d98b2b0
Matt Granmoe <mattgranmoe@gmail.com>
2017-05-21 20:14:40 -0500

    Added bump for physics

---
+++ TODO.md
@@ -0,0 +1,3 @@
+* Restart
+  * cleanup
+  * init
--- main.lua
+++ main.lua
@@ -1,7 +1,10 @@
-lurker = require('lurker/lurker')
+local lurker = require('dependencies.dev.lurker.lurker')
+local bump = require('dependencies.bump')
 
 local lg = love.graphics
 local lk = love.keyboard
+local world = bump.newWorld(64)
+
 local paddle = {}
 local ball = {}
 local bricks = {}
@@ -16,12 +19,17 @@ function love.load()
   paddle.height = height / 40
   paddle.x = (width / 2) - (paddle.width / 2)
   paddle.y = height - (paddle.height * 3)
-  paddle.vx = 0
+  paddle.isPaddle = true
+
   ball.radius = width / 100
   ball.x = (width / 2) - (ball.radius / 2)
   ball.y = (height / 2) - (ball.radius / 2)
-  ball.vy = 0
+  ball.vy = 100
   ball.vx = 0
+  ball.isBall = true
+
+  world:add(paddle, paddle.x, paddle.y, paddle.width, paddle.height)
+  world:add(ball, ball.x, ball.y, ball.radius, ball.radius)
 
   local function createBrick(x, y)
     return {
@@ -30,7 +38,8 @@ function love.load()
       health = 100,
       r = math.floor(math.random()*256),
       g = math.floor(math.random()*256),
-      b = math.floor(math.random()*256)
+      b = math.floor(math.random()*256),
+      isBrick = true
     }
   end
 
@@ -40,16 +49,46 @@ function love.load()
   local nextY = brickHeight * 2
 
   for i = 0, 29, 1 do
-    table.insert(bricks, createBrick(nextX, nextY))
+    local brick = createBrick(nextX, nextY)
+    world:add(brick, brick.x, brick.y, brickWidth, brickHeight)
+    table.insert(bricks, brick)
     nextX = nextX + brickWidth
     if (nextX == width) then nextX = 0 end
     if i == 10 then nextY = nextY + brickHeight end
   end
 end
 
+function collideBall (ball, dt)
+  local function ballFilter (other)
+    return 'bounce'
+  end
 
-function love.update()
+  local goalX, goalY = ball.x + (ball.vx * dt), ball.y + (ball.vy * dt)
+  local actualX, actualY, cols, len = world:move(ball, goalX, goalY, ballFilter)
+  ball.x, ball.y = actualX, actualY
+
+  for i=1, len do
+    local other = cols[i].other
+    if other.isBrick then
+      other.health = other.health - 50
+    elseif other.isPaddle then
+      -- adjust ball.vx based on where it hit the paddle
+      -- compensate for corner collisions that bump handled
+    -- elseif other.isFloor
+      -- lose life or lose game
+    -- elseif other.isWall
+      -- do stuff
+    end
+  end
+end
+
+function love.update(dt)
   lurker.update()
+
+  collideBall(ball, dt)
+  -- removeBricks (from bricks table and from world)
+  -- updateGameProgress (win/lose/lives/score)
+
   local function clampPosition (obj)
     local maxHeight = height - obj.height
     local maxWidth = width - obj.width
@@ -68,10 +107,6 @@ function love.update()
   end
 
   clampPosition(paddle)
--- collide ball with walls
-  -- if it hits 'floor' lose game
--- collide ball with bricks
--- collide ball with paddle
 end
 
 
@@ -79,9 +114,9 @@ function love.draw()
   lg.setColor(255, 255, 255)
   lg.rectangle('fill', paddle.x, paddle.y, paddle.width, paddle.height)
   lg.circle('fill', ball.x, ball.y, ball.radius)
-  -- draw bricks
+
   for _, brick in pairs(bricks) do
-    lg.setColor(brick.r, brick.g, brick.b)
+    lg.setColor(brick.r, brick.g, brick.b, (255 * (brick.health / 100)))
     lg.rectangle('fill', brick.x, brick.y, brickWidth, brickHeight)
   end
 end
